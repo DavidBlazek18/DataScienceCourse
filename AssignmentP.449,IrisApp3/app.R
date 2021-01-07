7619a933a757c6d9a4567f61b6db3db60a317c07
DavidBlazek18 <67991822+DavidBlazek18@users.noreply.github.com>
2021-01-07 18:26:23 -0500

    Add files via upload
    Please grade assignment, P.449 of Data Science course.

--- AssignmentP.449,IrisApp3/app.R
+++ AssignmentP.449,IrisApp3/app.R
@@ -1,50 +1,52 @@
 library(shiny)
 library(ggplot2)
 
-data <- iris
+
 
 ui <- fluidPage(
-  titlePanel("IrisDataSet"),
+  titlePanel(h1("Plot of Sepal Length vs. Width")),
   
-  sidebarLayout(position = "right"),
-      sidebarPanel(h1("Iris species")),
-                   img(src = "irisAll3SpeciesCombined.png", height = 140, width = 420),
-                   br(),
-                   br(),
-                helpText("Use the following to study data from the Fischer/Anderson Iris Data Set."),
-                br(),
-      
-      checkboxGroupInput("species", 
-                  label = "Choose each species you are studying:",
-                  choices = c("Setosa" = "Setosa", 
-                              "Versicolor" = "Versicolor", 
-                              "Virginica" = "Virginica"),
-                  selected = NULL),
-      
-      checkboxGroupInput("data_points", 
-                  label = "Data Points",
-                  ggplot(data=data, colour="red"))
-                 
+  sidebarLayout(position = "right",
+  sidebarPanel(h1("Iris Species & Parts"),
+  img(src = "irisAll3SpeciesCombined.png", height = 140, width = 420),
+  helpText("Use the following to study measurements from the Fischer/Anderson Iris Data Set."),
 
-    )
-    # aes_string(x=input$variable, y=input$variable2, 
-    mainPanel(
-      plotOutput("selected_species"),
-      plotOutput("selected_data_points")
-    )
+  radioButtons("species", 
+                     label = "Choose the species of Iris to study:",
+                     choices = c("Setosa" = "Setosa", 
+                                 "Versicolor" = "Versicolor", 
+                                 "Virginica" = "Virginica"),
+                     selected = NULL)),
   
+  mainPanel(
+    plotOutput("selected_species"),
+  
+)
+)
+)
+
 
 
 server <- function(input, output) {
   
+
   output$selected_species <- renderPlot({ 
-    ("You have selected" + input$species)
-  })
+    data <- switch(input$species,
+                   "Setosa" = iris[iris$Species == "setosa", ],
+                   "Versicolor" = iris[iris$Species == "versicolor", ],
+                   "Virginica" = iris[iris$Species == "virginica", ])
   
-  output$selected_data_points <- renderPlot({ 
-    ("You have chosen a range that goes from" + input$data_points)
+    
+    
+    ggplot(data=data, aes(x=Sepal.Length, y=Sepal.Width)) + geom_point()
+ #  g <- g+labs(x="A", y="B")                                               How can I change the labels to make them
+ #  g                                                                       different from the column headers?
+ #                                                                          This was from a tutorial, but doesn't work.
+   
+    
   })
-  
+ 
+
 }
 
 shinyApp(ui, server)
